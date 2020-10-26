7b45071206b6de515004d1d194e5e7a2b079a238
Sanoj Vaidyan <sanoj.vaidyan@rackspace.com>
2020-10-26 23:05:36 +0530

    Django REST Framework Study

---
+++ djangoapi/courses/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .views import CourseView
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('courses', CourseView)
+
+urlpatterns = router.urls
